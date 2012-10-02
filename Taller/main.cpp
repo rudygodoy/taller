644ad6c6aa78258b9e034a2d04733cc8e149a50e
Rudy Godoy <rudygodoyg@gmail.com>
2012-10-01 20:27:12 -0500

    Qt UI new main

--- Taller/main.cpp
+++ Taller/main.cpp
@@ -1,14 +1,11 @@
 #include <QtGui/QApplication>
-#include "qmlapplicationviewer.h"
+#include "mainwindow.h"
 
-Q_DECL_EXPORT int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    QScopedPointer<QApplication> app(createApplication(argc, argv));
-
-    QmlApplicationViewer viewer;
-    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-    viewer.setMainQmlFile(QLatin1String("qml/Taller/main.qml"));
-    viewer.showExpanded();
-
-    return app->exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    
+    return a.exec();
 }
