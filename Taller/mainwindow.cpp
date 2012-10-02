1837cb02d6e670799e5abce4b9f3e451ff6ce4f3
Rudy Godoy <rudygodoyg@gmail.com>
2012-10-02 14:24:30 -0500

    Implementación de formulario principal y menús
    Implementado menú principal y signals.

--- Taller/mainwindow.cpp
+++ Taller/mainwindow.cpp
@@ -1,3 +1,4 @@
+#include <QtGui>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -6,9 +7,21 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+// Inicializa ventana principal y define titulo
+    this->setWindowTitle(tr("Speed Factory"));
+    this->resize(480, 320);
+
+    connect( ui->pushButton_BuscarCliente, SIGNAL( clicked() ), this, SLOT( about() ) );
+    connect( ui->actionAbout, SIGNAL( triggered() ), this, SLOT ( about() ) );
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::about(){
+    QMessageBox::about(this, "About Speed Factory", tr("Esta aplicación fue ble bla"));
 }
--- Taller/mainwindow.h
+++ Taller/mainwindow.h
@@ -15,6 +15,10 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     
+public slots:
+//    void clienteBuscar();
+    void about();
+
 private:
     Ui::MainWindow *ui;
 };
